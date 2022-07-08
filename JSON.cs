41f077e33d799acad71e097e9eea18127382189e
Gaeus Caskie <fabrogaeuscaskie@gmail.com>
2022-07-08 22:45:55 +0800

    third-commit
    *added feature to create JSON Files
    *provided folders for each separate file types

---
+++ JSON.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contact_tracing
+{
+    internal class JSON
+    {
+        public string yourName { get; set; }
+        public string yourPhone { get; set; }
+        public string yourEmail { get; set; }
+        public string yourAddress { get; set; }
+        public string yourWebsite { get; set; }
+        public string skill1 { get; set; }
+        public string skill2 { get; set; }
+        public string skill3 { get; set; }
+        public string skill4 { get; set; }
+        public string dateOfWork1 { get; set; }
+        public string position1 { get; set; }
+        public string companyName1 { get; set; }
+        public string work1CompanySkill1 { get; set; }
+        public string work1CompanySkill2 { get; set; }
+        public string dateOfWork2 { get; set; }
+        public string position2 { get; set; }
+        public string companyName2 { get; set; }
+        public string work2CompanySkill1 { get; set; }
+        public string work2CompanySkill2 { get; set; }
+        public string dateOfWork3 { get; set; }
+        public string position3 { get; set; }
+        public string companyName3 { get; set; }
+        public string work3CompanySkill1 { get; set; }
+        public string work3CompanySkill2 { get; set; }
+        public string collegeLocation { get; set; }
+        public string collegeName { get; set; }
+        public string degreeObtained { get; set; }
+        public string SHSLocation { get; set; }
+        public string SHSName { get; set; }
+        public string strand { get; set; }
+        public string JHSLocation { get; set; }
+        public string JHSName { get; set; }
+        public string certificate1 { get; set; }
+        public string certificate2 { get; set; }
+        public string certificate3 { get; set; }
+
+    }
+}
